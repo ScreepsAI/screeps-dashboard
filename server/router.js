cf6b8f4ac1acd4b0f336b4079b6d5981ff6e90aa
canisminor1990 <canisminor1990@gmail.com>
2018-04-26 14:26:15 +0800

    update

--- server/router.js
+++ server/router.js
@@ -1,50 +1,56 @@
-const express  = require('express');
-const router   = express.Router();
+const express = require('express');
+const router = express.Router();
 const FileSync = require('lowdb/adapters/FileSync');
-const lowdb    = require('lowdb');
-const db       = lowdb(new FileSync('server/db.json'));
+const lowdb = require('lowdb');
+const db = lowdb(new FileSync('server/db.json'));
 
-router.get('/', function (req, res, next) {
-	const all = db.get('count').value();
-	res.json({
-		         err : 0,
-		         msg : '',
-		         data: all
-	         });
+router.get('/', function(req, res, next) {
+  const all = db.get('count').value();
+  res.json({
+    err: 0,
+    msg: '',
+    data: all,
+  });
 });
 
 router.get('/stats', (req, res, next) => {
-	res.json({
-		         err : 0,
-		         msg : '',
-		         data: db.get('stats').value()
-	         });
+  res.json({
+    err: 0,
+    msg: '',
+    data: db.get('stats').value(),
+  });
 });
 
 router.get('/stats/*', (req, res, next) => {
-	const url = req.url.toString().replace(/^\//,'').replace(/\//g,'.')
-	res.json({
-		         err : 0,
-		         msg : '',
-		         data: db.get(url).value()
-	         });
+  const url = req.url
+    .toString()
+    .replace(/^\//, '')
+    .replace(/\//g, '.');
+  res.json({
+    err: 0,
+    msg: '',
+    data: db.get(url).value(),
+  });
 });
 
 router.get('/graph', (req, res, next) => {
-	res.json({
-		         err : 0,
-		         msg : '',
-		         data: db.get('graph').value()
-	         });
+  res.json({
+    err: 0,
+    msg: '',
+    data: db.get('graph').value(),
+  });
 });
 
 router.get('/graph/*', (req, res, next) => {
-	const url = req.url.toString().replace(/^\//,'').replace(/\//g,'.')
-	res.json({
-		         err : 0,
-		         msg : '',
-		         data: db.get(url).value()
-	         });
+  const url = req.url
+    .toString()
+    .replace(/^\//, '')
+    .replace(/\//g, '.');
+  res.json({
+    err: 0,
+    msg: '',
+    data: db.get(url).value(),
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const app     = express();
+const app = express();
 app.use((req, res, next) => {
-	res.set('Access-Control-Allow-Origin', '*');
-	res.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-	res.set('Access-Control-Allow-Headers', 'Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
-	next();
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.set(
+    'Access-Control-Allow-Headers',
+    'Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With'
+  );
+  next();
 });
 app.use('/', require('./router'));
 app.use((req, res, next) => {
-	var err    = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 const server = app.listen(9000, () => {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('example app listening at http://%s:%s', host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('example app listening at http://%s:%s', host, port);
 });
