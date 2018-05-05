f359c6952873905c90fba6895dacec5845d71258
canisminor1990 <i@canisminor.cc>
2018-05-05 13:44:29 +0800

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,19 @@
 import style from './index.scss';
-import { Logo, Menu } from '../';
+import { Logo, Menu, Svg } from '../';
 
 export default ({ badge }) => {
   return [
-    <Logo key="logo" />,
+    <a key="logo" href={`https://canisminor.cc`} target="_blank">
+      <Logo />
+    </a>,
     <div key="menu">
       <div className={style.content}>
         <div>Screeps Dashboard</div>
         <div className={style.username}>{USERNAME}</div>
       </div>
+      <a key="header-right" href={`https://screeps.com/a/#!/profile/${USERNAME}`} target="_blank">
+        <Svg.badge className={style.avatar} content={badge} size="38" />
+      </a>
       <Menu
         content={[
           { title: 'Room', to: '/' },
